01e0796a16f62516639e57a388a3a528570d97ee
Jairo <jairocharnoski@gmail.com>
2026-01-19 19:15:40 -0300

    Criada OrderItem e OrderItemPK

--- src/main/java/com/charnoski/ecommerce/entities/Product.java
+++ src/main/java/com/charnoski/ecommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.charnoski.ecommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -22,9 +23,11 @@ public class Product {
     @ManyToMany
     @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id")
-    )
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -79,5 +82,13 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOrders() {
+        return items.stream().map(x -> x.getOrder()).toList();
     }
 }
